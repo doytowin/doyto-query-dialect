48e7cd203e5c1bcf9d44762d3c42eba3667eea20
Forb Yuan <f0rb@163.com>
2021-06-13 15:22:47 +0800

    add test MySQL8DialectTest#shouldNotWrapForNonKeywords

--- src/test/java/win/doyto/query/dialect/MySQL8DialectTest.java
+++ src/test/java/win/doyto/query/dialect/MySQL8DialectTest.java
@@ -10,10 +10,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @author f0rb on 2021-06-12
  */
 class MySQL8DialectTest {
+    MySQL8Dialect mySQL8Dialect = new MySQL8Dialect();
 
     @Test
     void wrapLabel() {
-        MySQL8Dialect mySQL8Dialect = new MySQL8Dialect();
         assertEquals("`rank`", mySQL8Dialect.wrapLabel("rank"));
+    }
+
+    @Test
+    void shouldNotWrapForNonKeywords() {
+        assertEquals("some_name", mySQL8Dialect.wrapLabel("some_name"));
     }
 }
